adf876349e0bd1cfa5394c35237e05e634d5e74f
Noushedul1 <noushedakib@gmail.com>
2021-12-29 13:11:35 +0600

    complete fetch api, use onMounted,res,reactive,computes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="text" v-model="firstname">
+  <input type="text" v-model="secondname">
+  <p>{{fullname}}</p>
+  <div v-for="book in books" :key="book.id">
+    <h1>{{book.id}}</h1>
+    <h2>{{book.title}}</h2>
+    <h3>{{book.author}}</h3>
+  </div>
+  <div v-for="all in alldata" :key="all.id" class="fetch">
+    <h4>{{all.title}}</h4>
+    <p>{{all.body}}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { computed, ref } from '@vue/reactivity'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const firstname = ref('akib');
+    const secondname = ref('islam');
+    const alldata = ref([]);
+    const books = ref([
+      {id:1 , title: 'web design', author: 'noushed'},
+      {id:2 , title: 'graphic design', author: 'akib'},
+      {id:3 , title: 'web development', author: 'islam'}
+    ]);
+    console.log(books);
+    const fullname = computed(()=>{
+      return `${firstname.value} ${secondname.value}`;
+    });
+    // onMounted(()=>{
+    //   fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(res=>res.json())
+    //   .then(data=>{
+    //     // console.log(data);
+    //     alldata.value = data.slice(0,10);
+    //   })
+    // })
+    (function fetchApi(){
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res=>res.json())
+      .then(data=>{
+        alldata.value = data.slice(0,10);
+      })
+    })()
+    return {
+      firstname,
+      secondname,
+      fullname,
+      books,
+      alldata
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fetch{
+  color: green;
+  border: 1px solid black;
+  background: #ddd;
+  padding: 10px;
+  margin: 10px;
 }
 </style>
